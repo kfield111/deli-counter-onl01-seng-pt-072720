03e91f8736e1d8f099d5db9341bfeee3785a4330
Kelin Field <kfield111@hotmail.com>
2020-08-04 01:45:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,14 @@ def line (array)
     puts "The line is currently empty."
     break
   end
+  new_array = []
   if array != []
     array.each_with_index do |item, index|
       position_in_line = index + 1
-      new_array = []
       new_array.push "#{position_in_line}. + #{item}"
     end
     first_puts = "The line is currently: "
-    puts first_puts
+    puts first_puts 
 
 
   end
