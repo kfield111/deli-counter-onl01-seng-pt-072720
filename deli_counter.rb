3839cfc28539a5692d263146c5744be0e5df51eb
Kelin Field <kfield111@hotmail.com>
2020-08-04 01:47:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,16 @@
 katz_deli = []
 
 def line (array)
-  while array == []
+  if array == []
     puts "The line is currently empty."
-    break
   end
   new_array = []
   if array != []
     array.each_with_index do |item, index|
       position_in_line = index + 1
-      new_array.push "#{position_in_line}. + #{item}"
+      new_array.push "#{position_in_line}. + #{item}."
     end
     first_puts = "The line is currently: "
     puts first_puts + new_array.join (" ")
-
-
   end
 end
