093f789571570b851986be5b2f5a5870b8bea346
Kelin Field <kfield111@hotmail.com>
2020-08-04 02:18:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,6 @@ def take_a_number (array, name)
   #binding.pry
   array.each_with_index do |item, index|
     number_in_line = index +1
-
-    puts "Welcome, #{name}. You are number #{number_in_line} in line."
+    puts "Welcome, #{item}. You are number #{number_in_line} in line."
   end
 end
