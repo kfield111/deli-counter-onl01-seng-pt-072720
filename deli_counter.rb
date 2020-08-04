92d91390a6e2bc76f006cc7b56fcda4dee6d7245
Kelin Field <kfield111@hotmail.com>
2020-08-04 02:32:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ end
 
 def now_serving (array)
   if array == []
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
 else puts "Currently serving #{array.first}"
   array.shift
 end
