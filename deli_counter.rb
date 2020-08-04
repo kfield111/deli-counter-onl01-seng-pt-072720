9ea4874e853093500cd0b033d3de9c68218252c4
Kelin Field <kfield111@hotmail.com>
2020-08-04 00:51:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,13 @@ def line (array)
   while array == []
     puts "The line is currently empty."
     break
-  end
   while array != []
     array.each_with_index do |item, index|
     spot_in_line = array.index + 1
     name = array.item
     puts "The line is currently: " + "#{spot_in_line}. #{name}."
     break
+  end
   end
 end
 
