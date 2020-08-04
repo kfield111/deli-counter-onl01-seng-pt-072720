d2d3d37a94aa88e8eecd7139bb7e4344aea5bdfd
Kelin Field <kfield111@hotmail.com>
2020-08-04 01:48:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line (array)
   if array != []
     array.each_with_index do |item, index|
       position_in_line = index + 1
-      new_array.push "#{position_in_line}. + #{item}."
+      new_array.push "#{position_in_line}. #{item}."
     end
     first_puts = "The line is currently: "
     puts first_puts + new_array.join(" ")
